08320188e2dea852bcead439140d7207709cf924
duongdong <duongdong3949@gmail.com>
2024-04-06 17:50:47 +0700

    Create signup.js and modify signup.html

---
+++ source/signup.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import {
+    getDatabase,
+    set,
+    ref,
+  } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyB9JtFcQE6GjT75vidQbbnfRhhUHAbXLQ8",
+    authDomain: "webapp-15dcd.firebaseapp.com",
+    databaseURL: "https://webapp-15dcd-default-rtdb.firebaseio.com",
+    projectId: "webapp-15dcd",
+    storageBucket: "webapp-15dcd.appspot.com",
+    messagingSenderId: "202280076352",
+    appId: "1:202280076352:web:f582a1afec4cb7cf608ba5"
+  };
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getDatabase(app);
+
+  let firstname = document.getElementById('fname-input');
+  let lastname = document.getElementById('lname-input');
+  let email = document.getElementById('email-input');
+  let password = document.getElementById('password-input');
+  let birthday = document.getElementById('birthday-input')
+  let mainform = document.getElementById('mainform');
+
+  let signUp = (event) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      let promises=[
+      set(ref(db, 'Users/' + userCredential.user.uid), {
+        role: "Students"
+      }),
+      set(ref(db, 'Students/' + userCredential.user.uid), {
+        email: email.value,
+        firstname: firstname.value,
+        lastname: lastname.value,
+        birthday: birthday.value,
+      })
+    ];
+      return Promise.all(promises);
+    })
+    .then(() => {
+      window.location.href = "./login.html"; 
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log(error.code);
+      console.log(error.message);
+    })
+  }
+  mainform.addEventListener('submit', signUp);
+
